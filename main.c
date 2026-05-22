d5ee5dde1b51ea828eb874ec69113c31d3e62920
Nicolas Tabet <nicolast654@gmail.com>
2026-05-23 02:11:59 +0300

    test: add a few test on main

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-    int *test  = malloc(sizeof(int) * 3);
-    // printf("malloced in main\n");
+    char *test = malloc(1);
+    char *real_test = realloc(test, 2);
+    void* freed = realloc(real_test, 0);
+
+    char *real_as_malloc = realloc(NULL, 10);
+    test = malloc(4);
+
     free(test);
+
+    char *calloc_test = calloc(12, 4);
     return 0;
 }
